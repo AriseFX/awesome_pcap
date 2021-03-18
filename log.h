4ecc3a23d118a14a7a0a58badeada55b23c02581
wziww <msckdbe@gmail.com>
2021-03-18 15:55:16 +0800

    init

--- log.h
+++ log.h
@@ -1,7 +1,6 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,22 +11,24 @@
  *  用来规划日志， 
  * 
  */
-#define log_err(...)  printf(__VA_ARGS__)
-#define log_info(...) printf(__VA_ARGS__)
+#define log_err(...) fprintf(stderr, __VA_ARGS__)
+#define log_info(...) fprintf(stdout, __VA_ARGS__)
 
 /*  利用调试开关， 控制调试信息的输出 */
 
 #if DPI_DEBUG
 
-#define log_dbg(...)  do {                      \
-        printf("%s:%s:%d\t", __FILE__, __func__, __LINE__); \
-        printf(__VA_ARGS__);                                    \
-}while(0)
+#define log_dbg(...)                                                \
+        do                                                          \
+        {                                                           \
+                printf("%s:%s:%d\t", __FILE__, __func__, __LINE__); \
+                printf(__VA_ARGS__);                                \
+        } while (0)
 
 #else
 
 #define log_dbg(...)
 
-#endif  /* DPI_DEBUG */
+#endif /* DPI_DEBUG */
 
-#endif  /* __LOG_H__ */
+#endif /* __LOG_H__ */
