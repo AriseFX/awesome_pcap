a050bc6f40c848e4a39f82b31c6c1c82b3cb14a6
wziww <msckdbe@gmail.com>
2021-03-22 14:51:04 +0800

    init

--- main.h
+++ main.h
@@ -40,7 +40,20 @@
 #ifdef PRO_TYPES_SSH
 
 #include "ssh.h"
+#include "./deps/src/cJSON/cJSON.h"
 
 #endif /* PRO_TYPES_SSH */
+
+struct g_prt_info_data
+{
+  u32_t pkt_count;  /*  用于统计文件中， 所有patcket的数量 */
+  u32_t ip_count;   /* IP 报文的数量， */
+  u32_t ipv4_count; /* IPV4报文的数量 */
+  u32_t ipv6_count; /* IPV6报文的数量 */
+  struct prt_info *head;
+  struct prt_info *tail;
+};
+
+struct g_prt_info_data _data;
 
 #endif /*__MAIN_H__ */
