dc6dd6bc12ceb669c9bd9f5834b00b11e6549005
wziww <msckdbe@gmail.com>
2021-03-29 17:50:16 +0800

    fix cmake

--- cmake/cJSON.cmake
+++ cmake/cJSON.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(cJSON
     PREFIX ${WROKDIR}/deps/
     GIT_REPOSITORY https://github.com/DaveGamble/cJSON.git
     GIT_TAG v1.7.14
-    CMAKE_COMMAND cmake .. -DENABLE_CJSON_UTILS=On -DENABLE_CJSON_TEST=Off -DCMAKE_INSTALL_PREFIX=/usr
+    CMAKE_COMMAND cmake .. -DENABLE_CJSON_UTILS=On -DENABLE_CJSON_TEST=Off -DCMAKE_INSTALL_PREFIX=./usr
 )   
 # .so dir set
 set(CJSON_LIB       ${WROKDIR}/deps/src/cJSON-build/libcjson.so)
